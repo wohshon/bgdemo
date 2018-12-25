f30e59421b552eb67e2e2f6004504a18105fdaee
Woh Shon <wohshon@gmail.com>
2018-12-25 09:08:26 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,25 +8,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../dist/img/favicon.ico">
     <!-- iPad retina icon -->
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="../dist/img/apple-touch-icon-precomposed-152.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="./dist/img/apple-touch-icon-precomposed-152.png">
     <!-- iPad retina icon (iOS < 7) -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../dist/img/apple-touch-icon-precomposed-144.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./dist/img/apple-touch-icon-precomposed-144.png">
     <!-- iPad non-retina icon -->
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="../dist/img/apple-touch-icon-precomposed-76.png">
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="./dist/img/apple-touch-icon-precomposed-76.png">
     <!-- iPad non-retina icon (iOS < 7) -->
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../dist/img/apple-touch-icon-precomposed-72.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./dist/img/apple-touch-icon-precomposed-72.png">
     <!-- iPhone 6 Plus icon -->
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="../dist/img/apple-touch-icon-precomposed-180.png">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="./dist/img/apple-touch-icon-precomposed-180.png">
     <!-- iPhone retina icon (iOS < 7) -->
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../dist/img/apple-touch-icon-precomposed-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./dist/img/apple-touch-icon-precomposed-114.png">
     <!-- iPhone non-retina icon (iOS < 7) -->
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="../dist/img/apple-touch-icon-precomposed-57.png">
-    <link href="../dist/css/patternfly.min.css" rel="stylesheet" media="screen, print">
-    <link href="../dist/css/patternfly-additions.min.css" rel="stylesheet" media="screen, print">
-    <script src="../components/jquery/dist/jquery.min.js"></script>
-    <script src="../components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="../components/datatables/media/js/jquery.dataTables.js"></script>
-    <script src="../dist/js/patternfly.min.js"></script>
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="./dist/img/apple-touch-icon-precomposed-57.png">
+    <link href="./dist/css/patternfly.min.css" rel="stylesheet" media="screen, print">
+    <link href="./dist/css/patternfly-additions.min.css" rel="stylesheet" media="screen, print">
+    <script src="./components/jquery/dist/jquery.min.js"></script>
+    <script src="./components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="./components/datatables/media/js/jquery.dataTables.js"></script>
+    <script src="./dist/js/patternfly.min.js"></script>
   </head>
   <body>
     <nav class="navbar navbar-default navbar-pf" role="navigation">
@@ -38,7 +38,7 @@
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="/">
-          <img src="../dist/img/brand.svg" alt="PatternFly Enterprise Application" />
+          <img src="./dist/img/brand.svg" alt="PatternFly Enterprise Application" />
         </a>
       </div>
       <div class="collapse navbar-collapse navbar-collapse-1">
@@ -90,7 +90,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="bluerose.jpeg">
+            <img src="./bluerose.jpeg">
           </p>
         </div>
       </div>
